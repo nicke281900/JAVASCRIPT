8c9b63b4a0667f8abc8f15c73abb3a0d3a503877
nicke281900 <126591579+nicke281900@users.noreply.github.com>
2024-09-11 08:32:15 -0300

    Add files via upload

---
+++ js alura1.js
@@ -0,0 +1,7 @@
+const texto1 ="Ola Mundo";
+const texto2 ="Ola Mundo";
+const senha = "senhasupersegura456";
+const stringdenumeros = "34567";
+
+const citação = 'Mu Nome é';
+const meuNome = 'Nicole' ;
