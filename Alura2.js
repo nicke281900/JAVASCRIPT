02b099cb67202e6f5839b0fd7e134017ef1efc3d
nicke281900 <126591579+nicke281900@users.noreply.github.com>
2024-09-11 09:08:33 -0300

    ok

---
+++ Alura2.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro programa;trbalho com variaveis")
+
+const idafde = 17;
+
+console.log (idade)
+console.log (idade+2)
+
+const idadesomada = idade+2;
+console.log(idadesomada);
