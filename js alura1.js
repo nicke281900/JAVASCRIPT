9248210d233780fc98afd18b3474b0fc548ec30e
nicke281900 <126591579+nicke281900@users.noreply.github.com>
2024-09-11 08:46:29 -0300

    ok

--- js alura1.js
+++ js alura1.js
@@ -3,5 +3,7 @@ const texto2 ="Ola Mundo";
 const senha = "senhasupersegura456";
 const stringdenumeros = "34567";
 
-const citação = 'Mu Nome é';
+const citação = 'Meu Nome é';
 const meuNome = 'Nicole' ;
+
+console.log(sitacao + meuNome)
