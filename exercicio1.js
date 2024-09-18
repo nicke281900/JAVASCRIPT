0e76e48f0e0d700a3e32e641bb1eb29086044f21
nicke281900 <126591579+nicke281900@users.noreply.github.com>
2024-09-18 08:57:50 -0300

    ok

--- exercicio1.js
+++ exercicio1.js
@@ -1,1 +1,15 @@
 // declaraçao de variaveis
+let numero1 = 10
+let numero2 = 5
+
+//operadores aritmeticos
+let soma = numero1 + numero2;
+let subtraçao = numero1 - numero2;
+let multoplicaçao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+//exibe os resultados no console
+console.log soma ("+soma");
+console.log subtraçao ("+subtracao");
+console.log multoplicaçao ("+multiplicacao");
+console.log divisao ("+divisao");
