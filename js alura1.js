8c466df3ce1c7118f412141a3d17f421346f5233
nicke281900 <126591579+nicke281900@users.noreply.github.com>
2024-09-25 08:22:07 -0300

    OK

--- js alura1.js
+++ js alura1.js
@@ -1,12 +1,12 @@
 console.log ("meu primeiro programa trabalhando com variaveis")
 
-cvonst idade = 17
+cvonst.idade = 17
 
-console log ("idade");
-console log ( idade );
-console log (idade+2);
-console log (idade-2);
-console log (idade/2);
+console.log ("idade");
+console.log ( idade );
+console.log (idade+2);
+console.log (idade-2);
+console.log (idade/2);
 
 const idadesoma =idade+2;
 const.log (idadesomada);
