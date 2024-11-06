0fd791a9280ecbf668e181082e1da8eed2b32c0a
nicke281900 <126591579+nicke281900@users.noreply.github.com>
2024-11-06 08:43:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,
+initial-scale=1.0">
+ <title>Catalogo de Filmes</title>
+<style>
+ body {
+    font-family: Arial, sans-serif;
+    pedding: 20px;
+    background-color: #f5f5f5;
+ }
+ h1 {
+     color: #333;
+ }
+label {
+    display: block;
+    margin-top: 10px;
+}
+input, textarea {
+    width: 100%;
+    padding: 8px;
+    margi-top: 5px;
+     margi-bottom: 10px;
+    border-radius: 5px;
+     border: 1px solid #ccc;
+}
+
+botton {
+    padding: 10px 15px;
+    background-color: #28a745;
+    color: whate;
+    border: nome;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-right: 10px;
+}
+ button:hover{
+     background-color: #218838;
+ }
